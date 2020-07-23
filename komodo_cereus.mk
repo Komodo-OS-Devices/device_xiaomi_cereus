c417cb5e06f1bceade0892d44a7d9d0648052e1d
wulan17 <galihgustip@gmail.com>
2020-07-23 13:06:43 +0700

    cereus: Komodo-ify
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- komodo_cereus.mk
+++ komodo_cereus.mk
@@ -18,11 +18,12 @@
 $(call inherit-product, device/xiaomi/cereus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+CURRENT_BUILD_TYPE := nogapps
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cereus
-PRODUCT_NAME := lineage_cereus
+PRODUCT_NAME := komodo_cereus
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 6
 PRODUCT_MANUFACTURER := Xiaomi
